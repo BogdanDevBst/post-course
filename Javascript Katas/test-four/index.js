66da103f2d5d8d9730517cd1d65b1efc11faa46e
BogdanDevBst <bn.workdev@gmail.com>
2020-04-29 14:21:34 +0100

    some changes

--- Javascript Katas/test-four/index.js
+++ Javascript Katas/test-four/index.js
@@ -2,7 +2,6 @@
 
 const myArray = [1, 2, 3, 4, 4, 5, 5, 6, 7, 8, 9];
 
-let duplicates = [...new Set(myArray)];
+console.log([...new Set(myArray)]);
 
-console.log(duplicates);
 
--- Javascript Katas/test-one/index.js
+++ Javascript Katas/test-one/index.js
@@ -1,16 +1,15 @@
 // 01. How do you find the missing numbers in a given integer array?
 
-let myArray = [1, 2, 3, 5, 6, 8, 9, 11, 12, 13, 14, 15, 17, 19]; // should output 0, 4, 7, 10, 16, 18
+const myIntegerArray = [1, 2, 3, 5, 6, 8, 9, 11, 12, 13, 14, 15, 17, 19];
 
-export const missingNumberInSequence = Math.max.apply(Math, myArray); // Because Math is not a constructor, max() is a static method of Math ( Always use it as Math.max(), rather than as a method of an instanced Math object).
+const missingNumbers = Math.max.apply(Math, myIntegerArray);
 
 let matchNum = [];
 
-for (i = 0; i < missingNumberInSequence; i++) {
-    if ( myArray.indexOf(i) < 0 ) {
+for (i = 0; i < missingNumbers; i++) {
+    if ( myIntegerArray.indexOf(i) < 0 ) {
         matchNum.push(i);
     }
-    return matchNum
 }
 
-// console.log(missingNumberInSequence);
+console.log(matchNum);
