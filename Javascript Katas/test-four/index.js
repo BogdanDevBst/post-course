d8ac974148bb439448027cd60fb4fc72712da6cd
BogdanDevBst <bn.workdev@gmail.com>
2020-04-29 16:07:33 +0100

    added 2nd solution to kata 4 test

--- Javascript Katas/test-four/index.js
+++ Javascript Katas/test-four/index.js
@@ -1,7 +1,19 @@
 // 04. How do you remove duplicates from an array in place?
 
+// using Set  // Set is a new data object and when you pass in an array, it will remove any duplicate values.
+
+
 const myArray = [1, 2, 3, 4, 4, 5, 5, 6, 7, 8, 9];
 
 console.log([...new Set(myArray)]);
+
+// using .filter
+
+const myArray = [1, 2, 3, 4, 4, 5, 5, 6, 7, 8, 9];
+
+const removeDuplicates = data => data.filter((value, index) => data.indexOf(value) === index);
+
+console.log(removeDuplicates(myArray));
+
 
 
